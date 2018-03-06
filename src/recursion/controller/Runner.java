8aee42f399573a1ce06951d5386dfda13a1bed20
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-06 12:13:01 -0700

    Added a runner.

---
+++ src/recursion/controller/Runner.java
@@ -0,0 +1,12 @@
+package recursion.controller;
+
+import recursion.view.Popup;
+
+public class Runner 
+{
+	public static void main (String [] args) 
+	{
+		Controller app = new Controller();
+		app.start();
+	}
+}
